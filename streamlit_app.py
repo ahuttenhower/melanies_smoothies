d62c98bbd56920ea9c76b66526952bea6f9bb3b4
ahuttenhower <annmarie@thehuttenhowers.com>
2025-07-31 10:28:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,7 @@ if ingredients_list: # this means, if the list is not null then do below
 
     for fruit_chosen in ingredients_list:
       ingredients_string += fruit_chosen + ' '# add this to what is already there
+      st.subheader(fruit_chosen + 'Nutrition Information')
       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
       sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
   
