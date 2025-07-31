538a048ebe40ca6d62818389cc7e17315a6c400b
ahuttenhower <annmarie@thehuttenhowers.com>
2025-07-31 09:40:11 -0500

    Update streamlit_app.py
    df

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,4 +50,5 @@ if ingredients_list: # this means, if the list is not null then do below
 # New sectino to displlay smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
